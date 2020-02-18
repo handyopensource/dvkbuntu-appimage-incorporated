409adefc4c2143e0802fee3cea955a56b0a36892
Paul WOISARD <paulwoisard@gmail.com>
2020-02-18 10:16:14 +0100

    ajout script pour ElectronPlayer dans votre système

---
+++ electronplayerinyoursystem.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+wget "https://github.com/handyopensource/dvkbuntu-appimage-incorporated/raw/master/electronplayer.png" -O "$HOME/.icons/electronplayer.png"
+
+wget "https://github.com/oscartbeaumont/ElectronPlayer/releases/download/v2.0.8-rc2/electronplayer-2.0.8.AppImage" -o "$HOME/AppImage/electronplayer-2.0.8.AppImage"
+
+chmod +x electronplayer-2.0.8.AppImage
+
+cat << FIN > "$HOME/.local/share/applications/electronplayer.desktop"
+[Desktop Entry]
+Comment[fr_FR]=
+Comment=
+Exec=$HOME/AppImage/electronplayer-2.0.8.AppImage
+GenericName[fr_FR]=Application pour voir des programmes en streaming
+GenericName=Watch streaming site in one app
+Icon=$HOME/.icons/electronplayer.png
+MimeType=
+Name[fr_FR]=ElectronPlayer
+Name=ElectronPlayer
+Path=$HOME
+Categories=Internet;Utility;
+StartupNotify=true
+Terminal=false
+TerminalOptions=
+Type=Application
+X-DBUS-ServiceName=
+X-DBUS-StartupType=
+X-KDE-SubstituteUID=false
+X-KDE-Username=
+FIN
